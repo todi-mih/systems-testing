0abf9e6f1d7efd3792ca59daf479c2a4921728dd
Theodhoraq Mihallari <tmihallari@gmail.com>
2025-04-30 11:26:56 +0300

    3

--- laboratories/cicd-documentation/test_tree.py
+++ laboratories/cicd-documentation/test_tree.py
@@ -1,97 +1,27 @@
+import pytest
 from node import Node
+from tree import Tree
 
+@pytest.fixture
+def setup_tree():
+    """ Fixture to set up a tree for testing """
+    tree = Tree()
+    tree.add(10)
+    tree.add(5)
+    tree.add(15)
+    tree.add(3)
+    tree.add(7)
+    tree.add(12)
+    tree.add(18)
+    return tree
 
-class Tree:
-    """ Tree class for binary tree """
+def test_find_existing_node(setup_tree):
+    """ Test that the find method returns the correct node when the data exists """
+    node = setup_tree.find(7)
+    assert node is not None 
+    assert node.data == 7  
 
-    def __init__(self):
-        """ Constructor for Tree class """
-        self.root = None
-
-    def getRoot(self):
-        """ Method for get root of the tree """
-        return self.root
-
-    def add(self, data):
-        """ Method for add data to the tree """
-        if self.root is None:
-            self.root = Node(data)
-        else:
-            self._add(data, self.root)
-
-    def _add(self, data, node):
-        """Method for add data to the tree
-
-        Args:
-            data (int): data to add
-
-        Returns:
-            None
-        """
-        if data < node.data:
-            if node.left is not None:
-                self._add(data, node.left)
-            else:
-                node.left = Node(data)
-        else:
-            if node.right is not None:
-                self._add(data, node.right)
-            else:
-                node.right = Node(data)
-
-    def find(self, data):
-        """Method for find data in the tree
-
-        Args:
-            data (int): data to find
-
-        Returns:
-            Node: node with data
-        """
-        if self.root is not None:
-            return self._find(data, self.root)
-        else:
-            return None
-
-    def _find(self, data, node):
-        if data == node.data:
-            return node
-        elif (data < node.data and node.left is not None):
-            return self._find(data, node.left)
-        elif (data > node.data and node.right is not None):
-            return self._find(data, node.right)
-
-    def deleteTree(self):
-        self.root = None
-
-    def printTree(self):
-        if self.root is not None:
-            self._printInorderTree(self.root)
-        print()
-        self._printPreorderTree(self.root)
-        print()
-        self._printPostorderTree(self.root)
-
-    def _printInorderTree(self, node):
-        if node is not None:
-            self._printInorderTree(node.left)
-            print(str(node.data) + ' ')
-            self._printInorderTree(node.right)
-
-    def _printPreorderTree(self, node):
-        # TODO
-        if node is not None:
-            print(str(node.data) + ' ')
-            self._printPreorderTree(node.left)
-            self._printPreorderTree(node.right)
-
-
-    def _printPostorderTree(self, node):
-        # TODO
-        if node is not None:
-            self._printPostorderTree(node.left)
-            self._printPostorderTree(node.right)
-            print(str(node.data) + ' ')
-
-
-
+def test_find_non_existing_node(setup_tree):
+    """ Test that the find method returns None when the data does not exist """
+    node = setup_tree.find(20)
+    assert node is None 
